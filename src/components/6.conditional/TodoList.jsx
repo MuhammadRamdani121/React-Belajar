b2999f6ddcddcb3fc50e4aad432e3ed89a6b14a2
MuhammadRamdani121 <muhamadramdani121.mr60@gmail.com>
2026-03-12 20:56:50 +0700

    Menambahkan Tenary Operation

--- src/components/6.conditional/TodoList.jsx
+++ src/components/6.conditional/TodoList.jsx
@@ -1,4 +1,5 @@
 import Todo from "../6.conditional/Todo";
+import TenaryOperation from "./tenary_Operator/TenaryOperator";
 export default function TodoList() {
   return (
     <>
@@ -7,6 +8,15 @@ export default function TodoList() {
         <Todo isCompleted={true} text="2.Learn Html" />
         <Todo isCompleted={true} text="3.Learn Html" />
         <Todo isCompleted={false} text="4.Learn Html" />
+        <br />
+        <TenaryOperation
+          isCompleted={true}
+          text="Leard Html"
+          isDeleted={true}
+        />
+        <TenaryOperation isCompleted={true} text="Leard Html" />
+        <TenaryOperation isCompleted={true} text="Leard Html" />
+        <TenaryOperation isCompleted={false} text="Leard Html" />
       </ul>
     </>
   );
---
+++ src/components/6.conditional/tenary_Operator/TenaryOperator.jsx
@@ -0,0 +1,15 @@
+export default function TenaryOperation({
+  text,
+  isCompleted,
+  isDeleted = false,
+}) {
+  if (isDeleted) {
+    return null;
+  } else {
+    return (
+      <>
+        <li>{isCompleted ? <del>{text}</del> : text}</li>
+      </>
+    );
+  }
+}
