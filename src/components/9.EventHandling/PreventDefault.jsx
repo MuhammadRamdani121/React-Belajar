1a899146eead0d6e2cbcd06311fb6599912d6216
MuhammadRamdani121 <muhamadramdani121.mr60@gmail.com>
2026-03-15 19:48:08 +0700

    Menambahkan File Prevent Default

---
+++ src/components/9.EventHandling/PreventDefault.jsx
@@ -0,0 +1,20 @@
+export default function SearchForm() {
+  return (
+    <>
+      <form action="">
+        <input
+          type="text
+        "
+        />
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            alert("you search");
+          }}
+        >
+          Search
+        </button>
+      </form>
+    </>
+  );
+}
